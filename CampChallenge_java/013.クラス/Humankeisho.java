6da4b2e0162f3556e6fcbac278f96181ba4242d7
EXP-0017\guest1Day <eku.hrm@gmail.com>
2017-10-19 13:36:13 +0900

    クラスの継承の課題を提出

---
+++ CampChallenge_java/013.クラス/Humankeisho.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.camp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author guest1Day
+ */
+public class Humankeisho extends HttpServlet {
+
+    
+       //Humanクラスの宣言
+public class Human {
+    //フィールドの宣言
+    public String name = "";
+    public int age = 0;
+    
+    //メソッドの宣言
+   public void setHuman(String n,int a){
+       //引数をフィールドへ設定
+       this.name = n;
+       this.age = a;
+   }
+    
+   public void printHuman(){
+       
+       System.out.print("氏名" + name + "<br>" + "年齢" + age + "<br>");
+   }
+}
+
+ class Clear extends Human{
+     
+     public void clearhuman(){
+         
+         this.name = "";
+         this.age = 0;
+     }
+ }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
